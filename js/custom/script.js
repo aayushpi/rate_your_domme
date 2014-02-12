6ea6fe405b5e5f2e8b99fe955ffb6d0e2e2a4874
Aayush Iyer <aayushiyer@aayushs-mbp.att.net>
2014-02-12 00:18:41 -0800

    update for E

--- js/custom/script.js
+++ js/custom/script.js
@@ -2,10 +2,13 @@
  * scripts.js
  */
 (function($) {
-
+  var content = ['BDSM doesn&rsquo;t have to be scary (unless you&rsquo;re into that)','Too much Evanesence? We can fix that with pink'];
+  var arr = Math.floor((Math.random()*2));
   $(document).ready(function(){
-    $('#lead h2').delay(500).fadeIn(500);
+    $('#lead h2').html(content[arr]);
+    $('#lead h2').delay(1000).fadeIn(500, function(){
+      $('nav').fadeIn(500);
+    });
   });
-
 
 }(jQuery));
